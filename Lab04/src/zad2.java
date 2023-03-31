54204e1633435e600950ff2dee5cf1cea2f11e9e
Rolaski <jakubowskijjal1@gmail.com>
2023-03-31 20:24:24 +0200

    task start

---
+++ Lab04/src/zad2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class zad2
+{
+    public static void main(String[] args)
+    {
+        //kod right here
+    }
+
+    //METODA WPROWADZANIA Z KLAWIATURY
+    public static double InputIn()
+    {
+        Scanner input = new Scanner(System.in);
+        double number = input.nextDouble();
+        return number;
+    }
+}
