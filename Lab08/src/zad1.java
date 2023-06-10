f44a1b267f9df21c97099ce8f90e6aa7fb3e2f76
Jakub <jakubowskijjal1@gmail.com>
2023-06-10 20:22:05 +0200

    task 1 :+1:

---
+++ Lab08/src/zad1.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class zad1
+{
+    public static void main(String[] args)
+    {
+        Scanner inputIn = new Scanner(System.in);
+        Set<String> names = new HashSet<>();
+        String name;
+        final String noMoreNames = "-";
+        do
+        {
+            System.out.print("Enter name: ");
+            name = inputIn.next();
+            if(!name.equals(noMoreNames))
+            {
+                names.add(name);
+            }
+        }while(!name.equals(noMoreNames));
+
+        System.out.println("The unique number of names is: "+names.size());
+    }
+}
