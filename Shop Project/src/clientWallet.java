80f559138e59c56174bcd12fa48cb5c53abcb07d
Jakub <jakubowskijjal1@gmail.com>
2023-06-23 00:13:31 +0200

    adding the option of operations on funds

---
+++ Shop Project/src/clientWallet.java
@@ -0,0 +1,24 @@
+import javax.xml.crypto.Data;
+
+public class clientWallet
+{
+    public static void clientWalletCheck(String login)
+    {
+        int walletCatch;
+        DataBase wallet = new DataBase("SELECT wallet FROM jdbc.customers WHERE login ='"+login+"';");
+        walletCatch = wallet.walletUser();
+        System.out.println("Your current funds: "+walletCatch);
+    }
+    public static void clientWalletUpdate(String login)
+    {
+        //żeby wpłacić hajs trzeba wiedzieć ile użytkownik go ma
+        //więc sprawdzimy i dodamy nową kwotę a sumę wrzucamy do bazy
+        int walletCatch;
+        DataBase wallet = new DataBase("SELECT wallet FROM jdbc.customers WHERE login ='"+login+"';");
+        walletCatch = wallet.walletUser();
+
+        int walletUpdate = inputs.inputIntMessage("Enter the amount to be deposited: ");
+        DataBase walletInsert = new DataBase("UPDATE `jdbc`.`customers` SET `wallet` = '"+(walletCatch + walletUpdate)+"' WHERE (`login` = '"+login+"');");
+        walletInsert.productInput();
+    }
+}
