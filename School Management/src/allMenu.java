7e5f0d70ea58530df68d2fae02f4f7c03bf0be94
Jakub <jakubowskijjal1@gmail.com>
2023-07-14 18:19:52 +0200

    Starting a new project

---
+++ School Management/src/allMenu.java
@@ -0,0 +1,12 @@
+public class allMenu
+{
+    public static void mainMenu()
+    {
+        System.out.println("Choose what you want to log in to");
+        System.out.println("1) Student account");
+        System.out.println("2) Teacher account");
+        System.out.println("3) Principal account");
+        System.out.println("4) Exit from the program");
+        System.out.print("Option selection: ");
+    }
+}
---
+++ School Management/src/allScaners.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class allScaners
+{
+    public static int inputInt()
+    {
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+}
